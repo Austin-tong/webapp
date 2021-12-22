b0f69124a1729caad1d4035c3a26b4829ab64840
tonry <18801187365@163.com>
2021-12-22 18:08:03 +0800

    day3

--- www/handlers.py
+++ www/handlers.py
@@ -2,7 +2,7 @@ import re, time, json, logging, hashlib, base64, asyncio
 #import markdown2
 from coroweb import get, post
 from models import User, Comment, Blog, next_id
-from apis import APIValueError, APIResourceNotFoundError, APIError
+from apis import APIValueError, APIResourceNotFoundError, APIError, APIPermissionError
 from config import configs
 from aiohttp import web
 
@@ -132,3 +132,33 @@ async def api_register_user(*,email, name, passwd):
     r.content_type = 'application/json'
     r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
+
+@get('/manage/blogs/create')
+async def manage_create_blogs():
+    return {
+        '__template__': 'manage_blog_edit.html',
+        'id': '', 
+        'action': '/api/blogs'
+    }
+
+@get('/api/blogs/{id}')
+async def api_get_blog(*, id):
+    blog = await Blog.find(id)
+    return blog
+
+def check_admin(request):
+    if request.__user__ is None or not request.__user__.admin:
+        raise APIPermissionError()
+
+@post('/api/blogs')
+async def api_create_blog(request, *, name, summary, content):
+    check_admin(request)
+    if not name or not name.strip():
+        raise APIValueError('name', 'name cannot be empty.')
+    if not summary or not summary.strip():
+        raise APIValueError('summary', 'summary cannot be empty.')
+    if not content or not content.strip():
+        raise APIValueError('content','content cannot be empty.')
+    blog = Blog(user_id = request.__user__.id, user_name=request.__user__.name, user_image=request.__user__.image, name= name.strip(), summary=summary.strip(), content=content.strip())
+    await blog.save()
+    return blog
